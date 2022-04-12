ade0cbb2473970fa78070e72a6ccc7cfb48d7938
amogh <amdam2013@gmail.com>
2022-04-12 17:47:33 -0500

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from distutils.command.config import config
 import sys
 import random
 import json
@@ -7,13 +8,32 @@ from dataset import Dataset
 from model import Model
 from train import *
 
-if __name__ == "__main__":
+def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--load", type=str, default="")
     parser.add_argument("--max-epochs", type=int, default=10)
     parser.add_argument("--batch-size", type=int, default=256)
     parser.add_argument("--sequence-length", type=int, default=4)
     args = parser.parse_args()
+    return args
+
+def writeLyrics(lyricPredictions: list, configOptions: dict, wpl: int):
+    stanzas = configOptions['numStanzas']
+    linesPerStanza = configOptions['numLinesPerStanza']
+    wordsPerLine = wpl
+
+    i = 0
+    for _ in range(stanzas):
+        for _ in range(linesPerStanza):
+            for _ in range(wordsPerLine):
+                sys.stdout.write(lyrics[i] + " ")
+                i += 1
+            sys.stdout.write("\n")
+        sys.stdout.write("\n")
+    
+    
+if __name__ == "__main__":
+    args = getArgs()
 
     with open("poetics.json", "r") as cf:
         opts = json.load(cf)
@@ -23,7 +43,6 @@ if __name__ == "__main__":
     wordsPerLine = random.randint(
         opts["numWordsPerLine"][0], opts["numWordsPerLine"][1]
     )
-
     nextWords = stanzas * linesPerStanza * wordsPerLine
 
     dataset = Dataset(args)
@@ -36,11 +55,4 @@ if __name__ == "__main__":
 
     lyrics = predict(dataset, model, text="stare into the mirror", next_words=nextWords)
 
-    i = 0
-    for stanza in range(stanzas):
-        for line in range(linesPerStanza):
-            for word in range(wordsPerLine):
-                sys.stdout.write(lyrics[i] + " ")
-                i += 1
-            sys.stdout.write("\n")
-        sys.stdout.write("\n")
+    writeLyrics(lyrics, opts, wordsPerLine)
